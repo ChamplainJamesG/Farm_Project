9fc374b53712ab0bd21970a193d69ca291536a47
Jimmy <jimmy@jimmy.com>
2020-09-16 14:44:11 -0400

    First init

---
+++ VeryCoolFarmGame/Source/VeryCoolFarmGame/VeryCoolFarmGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "VeryCoolFarmGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VERYCOOLFARMGAME_API AVeryCoolFarmGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
